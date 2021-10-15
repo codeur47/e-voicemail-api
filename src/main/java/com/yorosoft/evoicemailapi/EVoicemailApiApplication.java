93e870fcf4af3158dc8cf92f55523de962a50dd2
Codeur47 <yoropapers@gmail.com>
2021-10-15 00:04:56 +0000

    first commit

---
+++ src/main/java/com/yorosoft/evoicemailapi/EVoicemailApiApplication.java
@@ -0,0 +1,13 @@
+package com.yorosoft.evoicemailapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EVoicemailApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EVoicemailApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yorosoft/evoicemailapi/EVoicemailApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yorosoft.evoicemailapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EVoicemailApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
